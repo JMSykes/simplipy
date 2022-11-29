3f31379b1264c183c168893191bcc8fae2cd6f1d
JMSykes <nyc.jmsykes@gmail.com>
2022-11-29 16:57:32 -0500

    Add fraction from decimal function

--- fractions.py
+++ fractions.py
@@ -10,3 +10,16 @@ def reduce_fraction(fraction,export=tuple):
             den = num/fraction
 
         return int(num),int(den)
+
+def fraction_from_decimal(decimal):
+    num = 1
+    den = float(num/decimal)
+
+    if den % 1 == 0:
+        return int(num),int(den)
+    else:
+        while den % 1 != 0:
+            num += 1
+            den = num/decimal
+
+        return int(num),int(den)
