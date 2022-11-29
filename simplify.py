ba45c5b0a2cc6edda2b4a4c16e8fae19a65d3e06
JMSykes <nyc.jmsykes@gmail.com>
2022-11-29 17:56:50 -0500

    Add parenthetical nesting to init

--- simplify.py
+++ simplify.py
@@ -6,6 +6,7 @@ class simplify:
         self.right_side = equation.split('=')[1].strip()
 
         var = ''
+
         for character in self.left_side:
             alph = 'abcdefghijklkmnopqrstuvwxyz'
             if character.lower() in alph:
@@ -13,4 +14,19 @@ class simplify:
             elif (character.lower() not in alph) and (len(var) > 0):
                 break
 
+        def parenthetic_contents(string):
+            stack = []
+            for i, c in enumerate(string):
+                if c == '(':
+                    stack.append(i)
+                elif c == ')' and stack:
+                    start = stack.pop()
+                    yield (len(stack), string[start + 1: i])
+
+
+        self.parts['left_side']['p'] = list(parenthetic_contents(self.left_side))
+        self.parts['right_side']['p'] = list(parenthetic_contents(self.right_side))
+
+
         self.variable = var
+
